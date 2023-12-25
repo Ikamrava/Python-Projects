b30efbbe87fde5b79d15f6a17bfd4cf58485c336
ikamrava <kamrava.iman@outlook.com>
2023-12-25 21:18:06 +0000

    love calculator

---
+++ love calculator.py
@@ -0,0 +1,26 @@
+print("Welcome to the love calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+combined= name1 + name2
+lower_name = combined.lower()
+
+t = lower_name.count("t")
+r = lower_name.count("r")
+u = lower_name.count("u")
+e = lower_name.count("e")
+true = t + r + u + e
+
+l = lower_name.count("l")
+o = lower_name.count("o")
+v = lower_name.count("v")
+e = lower_name.count("e")
+love = l + o + v + e
+
+love_score = int(str(true) + str(love))
+if (love_score < 10) or (love_score > 90):
+    print(f"Your score is {love_score}, you go together like coke and mentos.")
+elif (love_score >= 40) and (love_score <= 50):
+    print(f"Your score is {love_score}, you are alright together.")
+else:
+    print(f"Your score is {love_score}.")
+    

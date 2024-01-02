9bef603f4808d3df3a3d3fa0f78342f0e0c548ff
ikamrava <kamrava.iman@outlook.com>
2024-01-02 12:44:40 +0000

    rock,paper

---
+++ rock_paper_scissors.py
@@ -0,0 +1,58 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+print("Welcome to Rock, Paper, Scissors!")
+print("Choose your weapon: 0 for rock, 1 for paper, or 2 for scissors.")
+options = [rock,paper,scissors]
+player_choice = int(input("Your choice: "))
+print(options[player_choice])
+computer_choice = random.randint(0, 2)
+print("Computer chose:")
+print(options[computer_choice])
+
+if player_choice >= 3 or player_choice < 0: 
+  print("You typed an invalid number, you lose!") 
+elif player_choice == 0 and computer_choice == 2:
+  print("You win!")
+elif computer_choice == 0 and player_choice == 2:
+  print("You lose")
+elif computer_choice > player_choice:
+  print("You lose")
+elif player_choice > computer_choice:
+  print("You win!")
+elif computer_choice == player_choice:
+  print("It's a draw")
+
+        
+    
+    
+
+
+
+
+    

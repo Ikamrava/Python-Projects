c67ed9f4cc348937aa7b49b07e7d039c62616ce6
ikamrava <kamrava.iman@outlook.com>
2024-02-02 15:44:01 +0000

    game_over

--- pong_game/main.py
+++ pong_game/main.py
@@ -28,7 +28,14 @@ while game_is_on:
     Screen.update()
     ball.move()
     if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
+        ball.bounce_y()
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+        
+    if ball.xcor() > 380:
+        ball.reset_position()
+        game_is_on = False
+        print("Lose")
 
 
 

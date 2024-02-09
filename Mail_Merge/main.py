fe1a4e6469cd2d76e10fd14f6f13f93e3908e5ab
ikamrava <kamrava.iman@outlook.com>
2024-02-09 18:52:38 +0000

    mail merge with word format

--- Mail_Merge/main.py
+++ Mail_Merge/main.py
@@ -1,8 +1,25 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+import docx
+
+PLACE_HOLDER = "[name]"
+
+
+with open("Mail_Merge/Input/Names/invited_names.txt") as names:
+   name_list = names.readlines()
+
+
+template_doc = docx.Document("Mail_Merge/Input/Letters/starting_letter.docx")
+
+
+for name in name_list:
+   stripped_name = name.strip()
+
+
+   new_doc = docx.Document()
+
+
+   for paragraph in template_doc.paragraphs:
+       new_paragraph = paragraph.text.replace(PLACE_HOLDER, stripped_name)
+       new_doc.add_paragraph(new_paragraph)
+
+
+   new_doc.save(f"Mail_Merge/Output/ReadyToSend/letter_for_{stripped_name}.docx")

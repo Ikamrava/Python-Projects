b2525966e7dc8b8dcf1188759e50f5a63cd8a593
ikamrava <kamrava.iman@outlook.com>
2024-02-09 18:22:26 +0000

    update snake to keep the score in a seperate tct file

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -3,18 +3,30 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("score.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.hideturtle()
         self.penup()
         self.goto(0, 250)
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 16, "normal"))
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align="center", font=("Arial", 16, "normal"))
         
     
     def update_score(self):
         self.score += 1
         self.clear()
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 16, "normal"))
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align="center", font=("Arial", 16, "normal"))
         
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align="center", font=("Arial", 16, "normal"))
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("GAME OVER", align="center", font=("Arial", 16, "normal"))
+    
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("score.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align="center", font=("Arial", 16, "normal"))
+  
